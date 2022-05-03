7d19aae88439a7b9e0cb436fb920934cb3a11ffd
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-03 17:32:19 +0300

    Create task4_additional.py

---
+++ task4_additional.py
@@ -0,0 +1,31 @@
+from task4 import Stack
+
+
+def balanced_string(string):
+    stack = Stack()
+    for s in string:
+        if s == '(':
+            stack.push(s)
+        else:
+            if stack.size() == 0:
+                return False
+            stack.pop()
+    if stack.size() != 0:
+        return False
+    return True
+
+
+def postfix_form(stack1):
+    stack2 = Stack()
+    for element in stack1.stack:
+        if element == '+':
+            stack2.push(stack2.pop() + stack2.pop())
+        elif element == "*":
+            stack2.push(stack2.pop() * stack2.pop())
+        elif element == "=":
+            return stack2.stack[0]
+        else:
+            stack2.push(element)
+
+    return stack2.stack[0]
+
