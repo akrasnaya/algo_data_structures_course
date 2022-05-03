3a805cc0aa6f2631c881f7c199bf2dbb6eaed13b
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-03 16:15:17 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -8,14 +8,14 @@ class Stack:
     def pop(self):
         if self.size() != 0:
             x = self.stack[-1]
-            del self.stack[-1]
+            self.stack.pop()
             return x
         return None
 
     def push(self, value):
-        self.stack.insert(0, value)
+        self.stack.append(value)
 
     def peek(self):
         if self.size() != 0:
-            return self.stack[-1]
+            return self.stack[0]
         return None
