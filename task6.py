e3a132d7b8cecb2e92ac8fe9512da8e0be50660c
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-05 10:10:29 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -10,12 +10,16 @@ class Deque:
 
     def removeFront(self):
         if self.size() != 0:
+            x = self.deque[0]
             self.deque.remove(self.deque[0])
+            return x
         return None
 
     def removeTail(self):
         if self.size() != 0:
+            x = self.deque[-1]
             self.deque.pop()
+            return x
         return None
 
     def size(self):
