4f9ae17c399041f7cfa3164ce01a1fdf8473df32
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-10 21:26:56 +0300

    Create task10.py

---
+++ task10.py
@@ -0,0 +1,52 @@
+class PowerSet:
+
+    def __init__(self):
+        self.values = []
+
+    def size(self):
+        return len(self.values)
+
+    def put(self, value):
+        if value in self.values:
+            return None
+        self.values.append(value)
+
+    def get(self, value):
+        return value in self.values
+
+    def remove(self, value):
+        if value in self.values:
+            self.values.remove(value)
+            return True
+        return False
+
+    def intersection(self, set2):
+        set3 = PowerSet()
+        for value in self.values:
+            if value in set2.values:
+                set3.put(value)
+        return set3
+
+    def union(self, set2):
+        set3 = PowerSet()
+        for value in self.values:
+            set3.put(value)
+        for value in set2.values:
+            set3.put(value)
+        return set3
+
+    def difference(self, set2):
+        set3 = PowerSet()
+        for value in self.values:
+            if value not in set2.values:
+                set3.put(value)
+        return set3
+
+    def issubset(self, set2):
+        count = 0
+        for value in set2.values:
+            if value in self.values:
+                count += 1
+        if count == set2.size:
+            return True
+        return False
