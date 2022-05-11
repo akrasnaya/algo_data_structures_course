f69f401c98095a2237ca66257f1f81b7695c0f26
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-11 21:57:28 +0300

    Update task11.py

--- task11.py
+++ task11.py
@@ -2,7 +2,7 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.filter = bin(2 ** self.filter_len)[2:]
+        self.filter = 2 ** self.filter_len
 
     def hash1(self, str1):
         h = 1
@@ -12,7 +12,6 @@ class BloomFilter:
         return abs(h) % self.filter_len
 
     def hash2(self, str1):
-        # 223
         h = 1
         for c in str1:
             code = ord(c)
@@ -22,12 +21,11 @@ class BloomFilter:
     def add(self, str1):
         h1 = self.hash1(str1)
         h2 = self.hash2(str1)
-        self.filter[h1] = '1'
-        self.filter[h2] = '1'
+        self.filter = self.filter + 2 ** h1 + 2 ** h2
 
     def is_value(self, str1):
         h1 = self.hash1(str1)
         h2 = self.hash2(str1)
-        if self.filter[h1] == '1' and self.filter[h2] == '1':
+        if self.filter & 2 ** h1 == 2 ** h1 and self.filter & 2 ** h2 == 2 ** h2:
             return True
         return False
