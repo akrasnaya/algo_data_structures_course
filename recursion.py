3a875243565c8d55a762466abebe54784d0b0e0c
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-06-30 19:06:00 +0300

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -11,16 +11,17 @@ def sum_num(N):
     return N % 10 + sum_num(N // 10)
 
 def pop_len(X):
-    array = X
-    if (array == []):
+    if (X == []):
         return 0
-    array.pop(0)
-    return 1 + pop_len(array)
+    X.pop(0)
+    return 1 + pop_len(X)
 
 def palindrom(s):
     return len(s) < 2 or (s[0] == s[-1] and palindrom(s[1: -1]))
 
 def print_even_elements(X):
+    if len(X) == 0:
+        return
     if X[0] % 2 == 0 and len(X) > 0:
         print(X[0])
     if len(X) > 0:
@@ -31,28 +32,21 @@ def print_even_indexes(X):
         print(X[0])
         print_even_indexes(X[2:])
 
-def find_max(array):
-    if len(array) > 1:
-        max = find_max(array[1:])
-        if array[0] < max:
-            return max
-        return array[0]
-    elif len(array) == 1:
-        return array[0]
-
-def second_max(X):
+def find_second_max(X, max1=float('-inf'), max2=float('-inf')):
     array = X
-    max1 = find_max(array)
-    array.remove(max1)
-    if len(array) > 0:
-        return find_max(array)
+    if len(array) == 0:
+        return max2
+    if array[0] > max1:
+        max2 = max1
+        max1 = array[0]
+    return find_second_max(array[1:], max1, max2)
+
 
 def get_files(path, level = 1):
     for f in os.listdir(path):
-        if os.path.isdir(path + '/' + f):
-            print(os.listdir(path + '/' + f))
-            get_files(path + '/' + f, level + 1)
+        if os.path.isdir(path + os.sep + f):
+            print(os.listdir(path + os.sep + f))
+            get_files(path + os.sep + f, level + 1)
 
 def find_files(path):
     get_files(path)
-
