b8e34a266add0a7ec35b015c6a9d2f238e70da70
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-07-03 16:05:43 +0300

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -24,7 +24,6 @@ def print_even_elements(X):
         return
     if X[0] % 2 == 0 and len(X) > 0:
         print(X[0])
-    if len(X) > 0:
         print_even_elements(X[1:])
 
 def print_even_indexes(X):
@@ -32,13 +31,18 @@ def print_even_indexes(X):
         print(X[0])
         print_even_indexes(X[2:])
 
-def find_second_max(X, max1=float('-inf'), max2=float('-inf')):
+def find_second_max(X, max1=None, max2=None):
     array = X
+    if max1 is None and max2 is None:
+        max1 = array[0]
+        max2 = array[0]
     if len(array) == 0:
         return max2
-    if array[0] > max1:
+    if array[0] > max1 and max2 <= max1:
         max2 = max1
         max1 = array[0]
+    if array[0] < max1 and array[0] >= max2:
+        max2 = array[0]
     return find_second_max(array[1:], max1, max2)
 
 
@@ -50,3 +54,4 @@ def get_files(path, level = 1):
 
 def find_files(path):
     get_files(path)
+
