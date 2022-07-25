7154617d7c1834a75230297133c39a6f76299baa
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-07-25 10:54:52 +0300

    Create test_task15.py

---
+++ test_task15.py
@@ -0,0 +1,44 @@
+import unittest
+from task15 import *
+
+def create_full_tree_to_test():
+    tree = aBST(2)
+    tree.Tree = [10, 8, 12, 6, 9, 11, 14]
+    return tree
+
+def create_sparse_tree_to_test():
+    tree = aBST(2)
+    tree.Tree = [10, None, 12, None, 9, None, 14]
+    return tree
+
+def create_empty_tree_to_test():
+    tree = aBST(0)
+    tree.Tree = [None]
+    return tree
+
+class TestABST(unittest.TestCase):
+
+    def test_find_existing_key(self):
+        full_tree = create_full_tree_to_test()
+        self.assertEqual(full_tree.FindKeyIndex(12), 2)
+        sparse_tree = create_sparse_tree_to_test()
+        self.assertEqual(sparse_tree.FindKeyIndex(9), 4)
+
+    def test_find_unexisting_key_full_tree(self):
+        full_tree = create_full_tree_to_test()
+        self.assertEqual(full_tree.FindKeyIndex(15), None)
+        self.assertEqual(full_tree.FindKeyIndex(0), None)
+
+    def test_find_unexisting_key_sparse_tree(self):
+        sparse_tree = create_sparse_tree_to_test()
+        self.assertEqual(sparse_tree.FindKeyIndex(8), 0)
+        self.assertEqual(sparse_tree.FindKeyIndex(11), -2)
+        self.assertEqual(sparse_tree.FindKeyIndex(15), None)
+
+    def test_find_unexisting_key_empty_tree(self):
+        empty_tree = create_empty_tree_to_test()
+        self.assertEqual(empty_tree.FindKeyIndex(15), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
