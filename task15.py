9449f2a1acb8df9a6ecd27b4ae03de0a14918d5e
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-07-26 11:32:51 +0300

    Update task15.py

--- task15.py
+++ task15.py
@@ -3,7 +3,7 @@ class aBST:
     def __init__(self, depth):
         # правильно рассчитайте размер массива для дерева глубины depth:
         tree_size = 0
-        for i in range(depth + 1):
+        for i in range(depth + 2):
             tree_size += 2 ** i
         self.Tree = [None] * tree_size # массив ключей
 
