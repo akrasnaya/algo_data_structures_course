8c6ae2049b72ff26b5f702c48a0d40f9cdc7b776
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-07-26 12:17:27 +0300

    Update task15.py

--- task15.py
+++ task15.py
@@ -47,37 +47,16 @@ class aBST:
 
     def AddKey(self, key):
         # добавляем ключ в массив
+
         if self.Tree[0] is None:
             self.Tree[0] = key
             return 0
 
-        def search(node):
-            if node is None:
-                return -1
-            elif key == node:
-                ind = self.Tree.index(node)
-                return ind
-            elif key < node:
-                ind = self.Tree.index(node)
-                left = 2 * ind + 1
-                if left > len(self.Tree) - 1:
-                    return -1
-                if self.Tree[left] is None:
-                    self.Tree[left] = key
-                    return left
-                return search(self.Tree[left])
-            else:
-                ind = self.Tree.index(node)
-                right = 2 * ind + 2
-                if right > len(self.Tree):
-                    return -1
-                if self.Tree[right] is None:
-                    self.Tree[right] = key
-                    return right
-                return search(self.Tree[right])
-
         if self.FindKeyIndex(key) is not None:
-            if self.FindKeyIndex(key) > 0:
-                return self.FindKeyIndex(key)
-
-        return search(self.Tree[0])
+            if self.FindKeyIndex(key) < 0:
+                ind = -self.FindKeyIndex(key)
+            else:
+                ind = self.FindKeyIndex(key)
+            self.Tree[ind] = key
+            return ind
+        return -1
