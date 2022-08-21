bb9b92a01e9eea129491ff48696284e80bdbd306
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-08-21 17:10:39 +0300

    Create task19.py

---
+++ task19.py
@@ -0,0 +1,48 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        # ваш код добавления новой вершины
+        # с значением value
+        # в свободное место массива vertex
+        new_vertex = Vertex(v)
+        self.vertex[v] = new_vertex
+        pass
+
+        # здесь и далее, параметры v -- индекс вершины
+
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        # ваш код удаления вершины со всеми её рёбрами
+        self.vertex[v] = None
+        del self.m_adjacency[v]
+        for row in self.m_adjacency:
+            del row[v]
+
+        pass
+
+    def IsEdge(self, v1, v2):
+        # True если есть ребро между вершинами v1 и v2
+        if self.m_adjacency[v1][v2] == 1 and self.m_adjacency[v2][v1] == 1:
+            return True
+        return False
+
+    def AddEdge(self, v1, v2):
+        # добавление ребра между вершинами v1 и v2
+        self.m_adjacency[v1][v2], self.m_adjacency[v2][v1] = 1, 1
+        pass
+
+    def RemoveEdge(self, v1, v2):
+        # удаление ребра между вершинами v1 и v2
+        self.m_adjacency[v1][v2], self.m_adjacency[v2][v1] = 0, 0
+        pass
